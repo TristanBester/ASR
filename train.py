bf586631b5f8574eba766b7105a852e38177372c
Tristan <>
2021-01-12 23:44:20 +0200

    update

--- train.py
+++ train.py
@@ -1,12 +1,12 @@
 import torch
-from torch.cuda.amp import autocast, GradScaler
+#from torch.cuda.amp import autocast, GradScaler
 from tqdm import tqdm
 import wandb
 import torch.nn.functional as F
 
 def train_one_epoch(model, criterion, optimizer, data_loader, device, scheduler=None, logging=False):
     model = model.train()
-    scaler = GradScaler()
+    #scaler = GradScaler()
     pbar = tqdm(data_loader, position=0, leave=True, total=len(data_loader))
 
     for spectrograms, labels, label_lens in pbar:
@@ -15,15 +15,17 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, scheduler=
         labels_lens = label_lens.to(device)
         optimizer.zero_grad()
 
-        with autocast():
-            preds = model(spectrograms)
-            preds = F.log_softmax(preds, dim=2).permute(1,0,2)
-            pred_lens = torch.full(size=(preds.shape[1],), fill_value=preds.shape[0], dtype=torch.long).to(device)
-            loss = criterion(preds, labels, pred_lens, label_lens)
+        #with autocast():
+        preds = model(spectrograms)
+        preds = F.log_softmax(preds, dim=2).permute(1,0,2)
+        pred_lens = torch.full(size=(preds.shape[1],), fill_value=preds.shape[0], dtype=torch.long).to(device)
+        loss = criterion(preds, labels, pred_lens, label_lens)
 
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+        loss.backward()
+        optimizer.step()
+        #scaler.scale(loss).backward()
+        #scaler.step(optimizer)
+        #scaler.update()
 
         if logging:
             wandb.log({'train_loss':loss.item()})
