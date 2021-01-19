216bb09c4392f58e9393430d3c04d3ab3ae3d0fc
Tristan <>
2021-01-19 15:13:15 +0200

    update

--- conv_model_exp.py
+++ conv_model_exp.py
@@ -29,14 +29,16 @@ for epoch in range(1000):
     model.train()
     counter = 0
     epoch_loss = 0
-    for specs, labels, label_lens in data_loader:
+    for specs, labels, label_lens in pbar:
         specs = specs.to(device)
         labels = labels.to(device)
         label_lens = label_lens.to(device)
 
         optimizer.zero_grad()
         outputs = model(specs)
+        initial_out = outputs
         outputs = F.log_softmax(outputs, dim=2)
+        log_out = outputs
         output_lens = torch.full(size=(outputs.shape[0],), fill_value=outputs.shape[1],
                                     dtype=torch.long).to(device)
         outputs = outputs.permute(1, 0, 2)
@@ -52,7 +54,14 @@ for epoch in range(1000):
         if torch.isnan(loss):
             print('Loss is NaN: ')
             print('Labels: ', labels)
-            print('Outputs: ', outputs)
+            print('i: ', initial_out)
+            print('l: ', log_out)
+            print('init outputs: ')
+            for x in initial_out:
+                print(x)
+            print('log_out: ')
+            for x in log_out:
+                print(x)
             0/0
 
 
@@ -61,13 +70,34 @@ for epoch in range(1000):
         else:
             counter += 1'''
 
+
+        Labels:  tensor([[19, 20,  1, 25, 27, 20, 21, 14,  5,  4]], device='cuda:0')
+Outputs:  tensor([[[ -2.0903,  -1.5693,  -2.5245,  ...,  -4.3433, -19.4653, -16.1507]],
+
+        [[ -0.3200,  -2.8227, -14.2828,  ...,  -5.8927, -26.4086, -11.1849]],
+
+        [[ -1.5300,  -2.3656, -16.3346,  ..., -14.9188, -20.4127,  -2.7846]],
+
+        ...,
+
+        [[  0.0000,     -inf,     -inf,  ...,     -inf,     -inf,     -inf]],
+
+        [[  0.0000,     -inf,     -inf,  ...,     -inf,     -inf,     -inf]],
+
+        [[     nan,      nan,      nan,  ...,      nan,      nan,      nan]]],
+       device='cuda:0', grad_fn=<PermuteBackward>)
+Traceback (most recent call last):
+  File "conv_model_exp.py", line 56, in <module>
+    0/0
+ZeroDivisionError: division by zero
+
         #n += 1
         #if not torch.isnan(loss):
         #ave = incremental_average(ave, loss.item(), n)
         pbar.set_description(f'Loss - {loss.item()}')
     #print()
     #scheduler.step()
-    print(f'Epoch loss: {epoch_loss/1000}')
+    print(f'\nEpoch loss: {epoch_loss/1000}')
     print('Model predictions: ')
     model.eval()
     with torch.no_grad():
