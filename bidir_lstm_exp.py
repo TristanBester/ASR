6c2ceb8fc614946961d89d136ed7cfbb499a5f3c
Tristan <>
2021-01-24 16:38:01 +0200

    update

--- bidir_lstm_exp.py
+++ bidir_lstm_exp.py
@@ -13,7 +13,7 @@ dataset = SoundClipDataset(csv_path='short_clips.csv', data_root='short_clips')
 data_loader = DataLoader(dataset, batch_size=1, collate_fn=collate_fn)
 
 device = torch.device('cuda')
-model = BidirectionalLSTMdevice)
+model = BidirectionalLSTMdevice()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1,
                                                  patience=1, min_lr=10e-8, verbose=True)
